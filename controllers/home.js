6fc33948c0c5ea8d841212a780d2b96edc0605ef
davide <the--kage91@hotmail.it>
2021-01-24 15:26:23 +0100

    Updated

--- controllers/home.js
+++ controllers/home.js
@@ -1,21 +1,66 @@
 const _ = require("underscore");
 const request = require("request");
+const User = require("../models/User");
 
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
+  User.find()
+    .select("profile.name")
+    .select("profile.website")
+    .select("profile.picture")
+    .exec((err, users) => {
+      if (err) {
+        return;
+      }
+
+      console.log(users);
+
+      if (100 - users.length > 0) {
+        let options = {
+          url: `https://uifaces.co/api?limit=${200 - users.length}&random`,
+          headers: {
+            "X-API-KEY": "73188088aadd0280d27c736ca4beaa"
+          }
+        };
+
+        request(options, (err, response, body) => {
+          console.log([...JSON.parse(body), ...users]);
+          res.render("home", {
+            title: "Home",
+            users: [...JSON.parse(body), ...users]
+          });
+        });
+      } else {
+        res.render("home", {
+          title: "Home",
+          users: users
+        });
+      }
+
+      /*
+      request(options, (err, response, body) => {
+        res.render("home", {
+          title: "Home",
+          users: JSON.parse(body)
+        });
+      });*/
+    });
+
+  /*
   let options = {
     url: "https://uifaces.co/api?limit=200&random",
     headers: {
       "X-API-KEY": "73188088aadd0280d27c736ca4beaa"
     }
   };
+
   request(options, (err, response, body) => {
     res.render("home", {
       title: "Home",
       users: JSON.parse(body)
     });
-  });
+  });*/
 };
--- controllers/user.js
+++ controllers/user.js
@@ -157,11 +157,20 @@ exports.postUpdateProfile = (req, res, next) => {
     if (err) {
       return next(err);
     }
+
+    if (!req.body.email) {
+      user.isComplete = true;
+    } else {
+      user.isComplete = false;
+    }
+
     user.email = req.body.email || "";
     user.profile.name = req.body.name || "";
     user.profile.gender = req.body.gender || "";
     user.profile.location = req.body.location || "";
     user.profile.website = req.body.website || "";
+    user.profile.bio = req.body.bio || user.profile.bio;
+
     user.save(err => {
       if (err) {
         if (err.code === 11000) {
